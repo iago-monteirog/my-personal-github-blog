bb46e8285c237ac47414e80e564a265ddf04bb64
iago-monteirog <iagomgarcia9000@gmail.com>
2023-08-25 16:27:48 -0300

    wip: issues list home page

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -2,8 +2,32 @@ import { NavLink } from "react-router-dom";
 import { Profile } from "../../components/Profile";
 import { SearchForm } from "../../components/SearchForm";
 import * as S from './styles';
+import { useEffect, useState } from "react";
+import { api } from "../../api/api";
+
+interface User {
+    login: string,
+}
+
+interface Issue {
+    title: string,
+    user: User,
+    body: string,
+    number: number
+}
 
 export function BlogPage() {
+    const [issuesData, setIssuesData] = useState<Issue[]>([]);
+
+    async function loadIssuesList() {
+        const response = await api.get('/search/issues?q=repo:iago-monteirog/my-personal-github-blog');
+
+        setIssuesData(response.data.items);
+    }
+
+    useEffect(() => {
+        loadIssuesList();
+    }, []);
 
     return (
         <S.BlogContainer>
@@ -11,54 +35,22 @@ export function BlogPage() {
             <SearchForm />
 
             <S.BlogContent>
-                <NavLink to="/post">
-                    <S.Post>
-                        <S.PostHeader>
-                            <p>JavaScript data types and data structures</p>
-                            <span>Há 1 dia</span>
-                        </S.PostHeader>
+                {issuesData.map((issue) => {
+                    return (
+                        <NavLink to="/post" key={issue.number}>
+                            <S.Post>
+                                <S.PostHeader>
+                                    <p>{issue.title}</p>
+                                    <span>Há 1 dia</span>
+                                </S.PostHeader>
 
-                        <p>
-                            Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in...
-                        </p>
-                    </S.Post>
-                </NavLink>
-                <NavLink to="/post">
-                    <S.Post>
-                        <S.PostHeader>
-                            <p>JavaScript data types and data structures</p>
-                            <span>Há 1 dia</span>
-                        </S.PostHeader>
-
-                        <p>
-                            Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in...
-                        </p>
-                    </S.Post>
-                </NavLink>
-                <NavLink to="/post">
-                    <S.Post>
-                        <S.PostHeader>
-                            <p>JavaScript data types and data structures</p>
-                            <span>Há 1 dia</span>
-                        </S.PostHeader>
-
-                        <p>
-                            Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in...
-                        </p>
-                    </S.Post>
-                </NavLink>
-                <NavLink to="/post">
-                    <S.Post>
-                        <S.PostHeader>
-                            <p>JavaScript data types and data structures</p>
-                            <span>Há 1 dia</span>
-                        </S.PostHeader>
-
-                        <p>
-                            Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in...
-                        </p>
-                    </S.Post>
-                </NavLink>
+                                <p>
+                                    Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in...
+                                </p>
+                            </S.Post>
+                        </NavLink>
+                    )
+                })}
             </S.BlogContent>
         </S.BlogContainer>
     )
